b88c494a61e159649b976e42f69391bc11c4bb1f
Sumit Kushwaha <62748297+sumit024@users.noreply.github.com>
2024-04-20 18:24:01 +0000

    ADD: variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const userId=1234
+let userEmail= "sumit@yopmail.com"
+var userPassword = "Sumit@123"
+userCity="Varanasi"
+
+// userId=2333 (can not be modified)
+
+userEmail= "kush@yopmail.com"
+userCity="Mau"
+userPassword="Sumit@122344"
+console.table([userId,userEmail,userCity,userPassword])
+
+
