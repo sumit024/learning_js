9318cb68351f2140c137ce39bafb4e356747acec
Sumit Kushwaha <62748297+sumit024@users.noreply.github.com>
2024-04-23 17:30:08 +0000

    ADD: conversion datatypes

---
+++ 01_basics/03_coversion_datatypes.js
@@ -0,0 +1,46 @@
+let marks= "44abs"
+
+console.log(typeof marks);
+
+let marksInNumber= Number(marks)
+console.log(typeof marksInNumber)
+console.log(marksInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+let isLoggedIn = "sumit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sumit" => true
+
+// *********************** Operations ***********************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1= "hello"
+let str2= " sumit"
+//console.log(str1+str2);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1=num2=num3=2+2;
+console.log(num3);
