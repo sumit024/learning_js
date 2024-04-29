0a2d111897ba00aa6bf884b92c19019e8e9baa8c
Sumit Kushwaha <62748297+sumit024@users.noreply.github.com>
2024-04-29 17:33:52 +0000

    arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+const arr =[10,20,30,40,50]
+const arr2 = new Array(1,2,3,4)
+
+//console.log(arr);
+//console.log(arr2);
+
+// array methods
+//console.log(arr);
+//arr.push(7)
+//arr.pop()
+//arr.shift()
+//arr.unshift(9)
+//console.log(arr);
+
+// slice and splice//////
+// slice, splice
+
+console.log('Original array',arr);
+
+const slicedArr= arr.slice(1,3);
+console.log('after slicing array',slicedArr);
+console.log('array after slice',arr);
+
+const splicedArr= arr.splice(1,3);
+console.log('after splicing array',splicedArr);
+console.log('array after splice',arr);
